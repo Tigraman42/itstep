664bb6d6e2dfbaae5ac36b05293ce3f0eb29dece
Tigraman42 <mariannatigr06@gmail.com>
2023-11-18 17:55:58 +0200

    Change register to uppercase

---
+++ LogTest.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging msg: %(asctime)s:%(levelname)s - %(message)s")
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+#logging.critical("critical")
+
+try:
+    print(10/0)
+except Exception:
+    logging.exception("Exception")
