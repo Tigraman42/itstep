8c640e85cc0c1eb5f14fabf70d6d13b99556d145
Tigraman42 <mariannatigr06@gmail.com>
2023-11-21 16:14:41 +0200

    Change register to uppercase

--- HM_11(1)/home_work11(1).py
+++ HM_11(1)/home_work11(1).py
@@ -22,7 +22,7 @@ for (x, y, width, height) in cat_face:
     glasses = glasses.resize((width, int(height/3)))
     cat.paste(glasses, (x, int(y+height/3)), glasses)
     cat.save("cat`s_with_glasses.png")
-    cat_with_glasses = cv2.imread("cat`s_with_glasses.png")
+    cat_with_glasses = cv2.imread("../cat`s_with_glasses.png")
     cv2.imshow("Cat`s with glasses", cat_with_glasses)
     cv2.rectangle(image, (x, y), (x+width, y+height), (0,0,255), 3)
 
